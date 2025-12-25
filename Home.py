e458842477c25660ee86d57d3a47ffd29f0326c2
luyc1110-gif <luyc1110@gmail.com>
2025-12-25 17:01:57 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ st.markdown("""
 <style>
 /* 🔥 1. 這裡是【網頁大背景】的顏色 */
 .stApp {
-    background-color: #3f84eb !important; /* 建議用淺灰色，讓卡片浮起來 */
+    background-color: #bff2cf !important; /* 建議用淺灰色，讓卡片浮起來 */
     color: #f0f07f !important;
 }
 
